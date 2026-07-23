da89012e78df5a03508a84460d3ee1ccd252e339
hasan-dev <64647199+hasansigwsx8iw12@users.noreply.github.com>
2026-07-23 03:49:01 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,35 @@
 // Firebase Imports
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import { initializeFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 
 
 // Firebase Config
 
 const firebaseConfig = {
+
   apiKey: "AIzaSyBJ2hF5xw9QF7KEmNi6Vseu_t_TEzoZz8M",
+
   authDomain: "khlonetorder.firebaseapp.com",
+
   projectId: "khlonetorder",
+
   storageBucket: "khlonetorder.firebasestorage.app",
+
   messagingSenderId: "334428875974",
+
   appId: "1:334428875974:web:b6acfc7782c541b6e3bbc7",
+
   measurementId: "G-PXYS88ZM5T"
+
 };
+
 
 
 // Initialize Firebase
@@ -24,16 +37,21 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 
+
 // Realtime Database
 
 const db = getDatabase(app);
 
 
+
 // Firestore
 
-const firestore = getFirestore(app, {
-    experimentalAutoDetectLongPolling: true
+const firestore = initializeFirestore(app, {
+
+    experimentalForceLongPolling: true
+
 });
+
 
 
 // Authentication
@@ -41,10 +59,15 @@ const firestore = getFirestore(app, {
 const auth = getAuth(app);
 
 
+
 // Export
 
 export {
+
     db,
+
     firestore,
+
     auth
+
 };
