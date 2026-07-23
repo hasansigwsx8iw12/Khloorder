12899a97d9ace6700c046b3c29bec2b25175eba7
hasan-dev <64647199+hasansigwsx8iw12@users.noreply.github.com>
2026-07-23 03:56:07 +0300

    Update repairs.js

--- repairs.js
+++ repairs.js
@@ -1,14 +1,12 @@
-import { firestore } from "./firebase.js";
+import { db } from "./firebase.js";
 
 import {
-    collection,
-    getDocs,
-    deleteDoc,
-    doc,
-    updateDoc,
-    query,
-    orderBy
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+    ref,
+    onValue,
+    remove,
+    update
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
 
 
 const table = document.getElementById("dataTable");
@@ -17,24 +15,291 @@ let allData = [];
 
 
 
+
 // تحميل البيانات
-async function loadData(){
+
+function loadData(){
 
 
-try{
-
-
-const q = query(
-    collection(firestore,"maintenance"),
-    orderBy("createdAt","desc")
-);
-
-
-const snapshot = await getDocs(q);
+onValue(ref(db,"maintenance"),(snapshot)=>{
 
 
 allData=[];
 
+
+
+snapshot.forEach((item)=>{
+
+
+allData.push({
+
+id:item.key,
+
+data:item.val()
+
+});
+
+
+});
+
+
+
+showData(allData);
+
+
+
+});
+
+
+}
+
+
+
+
+
+
+
+// عرض البيانات
+
+function showData(data){
+
+
+table.innerHTML="";
+
+
+
+if(data.length===0){
+
+
+table.innerHTML=`
+
+<tr>
+
+<td colspan="8">
+
+لا توجد بيانات
+
+</td>
+
+</tr>
+
+`;
+
+return;
+
+}
+
+
+
+
+
+data.forEach((item)=>{
+
+
+let d=item.data;
+
+
+
+table.innerHTML += `
+
+<tr>
+
+
+<td>
+${d.employee || "غير معروف"}
+</td>
+
+
+<td>
+${d.type || ""}
+</td>
+
+
+<td>
+${d.name || ""}
+</td>
+
+
+<td>
+${d.national || ""}
+</td>
+
+
+<td>
+${d.tower || ""}
+</td>
+
+
+<td>
+${d.price || 0}
+</td>
+
+
+<td>
+${d.date || ""}
+</td>
+
+
+<td>
+
+
+<button onclick="editData('${item.id}')">
+تعديل
+</button>
+
+
+
+<button
+style="background:#e53935"
+onclick="deleteData('${item.id}')">
+
+حذف
+
+</button>
+
+
+</td>
+
+
+</tr>
+
+`;
+
+});
+
+
+}
+
+
+
+
+
+
+
+// البحث
+
+window.searchName=function(){
+
+
+let value=document
+.getElementById("searchName")
+.value
+.trim();
+
+
+
+if(value===""){
+
+showData(allData);
+
+return;
+
+}
+
+
+
+let result = allData.filter(item =>
+
+(item.data.name || "")
+.includes(value)
+
+);
+
+
+
+showData(result);
+
+
+};
+
+
+
+
+
+
+
+
+
+// حذف
+
+window.deleteData=async function(id){
+
+
+if(!confirm("هل تريد حذف هذا الطلب؟"))
+return;
+
+
+
+await remove(
+ref(db,"maintenance/"+id)
+);
+
+
+
+alert("تم الحذف");
+
+
+};
+
+
+
+
+
+
+
+
+
+// تعديل الاسم
+
+window.editData=async function(id){
+
+
+
+let item = allData.find(x=>x.id===id);
+
+
+
+if(!item)
+return;
+
+
+
+let newName = prompt(
+"تعديل الاسم",
+item.data.name
+);
+
+
+
+if(!newName)
+return;
+
+
+
+await update(
+
+ref(db,"maintenance/"+id),
+
+{
+
+name:newName
+
+}
+
+);
+
+
+
+alert("تم التعديل");
+
+
+};
+
+
+
+
+
+loadData();
 
 snapshot.forEach((item)=>{
 
